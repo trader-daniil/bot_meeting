e04b0a51f1c48d68d8b4b97264af45cd1604b0c6
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-05-25 18:03:05 +0300

    add redis functions

---
+++ data.py
@@ -0,0 +1,84 @@
+import redis
+import os
+from dotenv import load_dotenv
+import datetime
+
+
+def get_user_status(user_id, redis_con):
+    """Определяем статус учатника по его id Tg
+        Используем структуру данных SET."""
+    speakers_ids = redis_con.smembers('speakers')
+    orginizers_ids = redis_con.smembers('organizers')
+    if user_id in speakers_ids:
+        return 'Спикер'
+    elif user_id in orginizers_ids:
+        return 'Организатор'
+    return 'Слушатель'
+
+
+def add_speaker(speaker_name, speach_time, redis_con):
+    """Добавляем пользователя по его ID в TG как списка
+        Используем тип данных HASH ключ - время в формате HH:MM:SS значение - id спикера"""
+    redis_con.hset(
+        'speach_time',
+        speach_time,
+        speaker_name,
+    )
+
+def get_speaker_questions(speaker_name, redis_con):
+    """Получим все контакты тех, кто хочет задать вопрос докладчику
+        Используем тип данных SET."""
+    contacts = redis_con.smembers(f'{speaker_name}_contacts')
+    return contacts
+
+
+def get_current_speach(redis_con):
+    """Получим текущее выступление
+        Используем тип данных key-value, где value это время начала выступления
+        Затем обращаемся к этому времени и получаем подробную информацию о выступлении"""
+    speach_start_time = redis_con.get('current_speach')
+    current_speach = redis_con.hget(
+        'speach_time',
+        speach_start_time,
+    )
+    return current_speach
+
+def create_question(user_id, question, redis_con):
+    """Получим текущее выступление и зададим вопрос спикеру
+        Используем тип данных SET, ключ это <username спикера>_questions
+        и значение это перечисление вопросов."""
+    current_speaker = get_current_speach(redis_con=redis_con).decode('utf-8')
+    redis_con.sadd(
+        f'{current_speaker}_questions',
+        f'{user_id}: {question}',
+    )
+
+
+def get_speaker_questions(speaker_id, redis_con):
+    """Получим текущие вопросы к спикеру, вернем в формате
+        id задающего в ТГ: текст вопроса."""
+    questions = {}
+    contacts = redis_con.smembers(f'{speaker_id}_questions')
+    for question in contacts:
+        username, question = question.decode('utf-8').split(': ')
+        questions[username] = question
+    return questions
+
+
+def main():
+    load_dotenv()
+    tg_bot_token = os.getenv('TG_BOT_TOKEN')
+    r = redis.Redis(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        db=os.getenv('DB_NUMBER'),
+    )
+    print(get_speaker_questions(
+        speaker_id='Daniil',
+        redis_con=r
+    ))
+    
+
+    
+if __name__ == '__main__':
+    main()
